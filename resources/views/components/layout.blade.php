44940bfc7c2abacef0f35356fd6f85b54d084b26
David Delon <david.delon@clapas.net>
2024-05-31 15:35:40 +0000

    Tailwindcss statique

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,11 +7,7 @@
 
     <title>Mares</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
